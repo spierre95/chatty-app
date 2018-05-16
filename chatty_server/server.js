e16f03f939f993daf28e99ebac7cfefb1b4d2d30
spierre95 <stephen.pierre20@gmail.com>
2018-05-16 19:06:33 +0000

    updated server to broadcast all

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -6,6 +6,8 @@ const SocketServer = require('ws').Server;
 // Set the port to 3001
 const PORT = 3001;
 
+
+
 // Create a new express server
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
@@ -15,6 +17,12 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+      client.send(data);
+  });
+};
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
@@ -23,11 +31,9 @@ wss.on('connection', (ws) => {
 
 ws.on('message', function incoming(data) {
   const fromClient = JSON.parse(data)
-  fromClient['key'] = uuidv4();
-  ws.send(JSON.stringify(fromClient))
+  fromClient['id'] = uuidv4();
+  wss.broadcast(JSON.stringify(fromClient))
 });
-
-
 
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
